2064c458d97f5f2b0a4a7d5e9c94a5870dd97f70
gitLMgit <115313459+gitLMgit@users.noreply.github.com>
2024-11-07 12:05:13 +0100

    Update RegularPolygon.java

--- src/topic4_vector_graphics/RegularPolygon.java
+++ src/topic4_vector_graphics/RegularPolygon.java
@@ -20,12 +20,20 @@ public class RegularPolygon implements Drawing {
 	@GadgetDouble(min = 0, max = 1)
 	double alpha = 0.0;                        // Ugao pocetnog temena (u okretima)
 	
+	
 
+	Vector getVertex(int i) {                  // Vraca polozaj i-tog temena
+		return Vector.polar(r, alpha + 1.0 * i / n);
+	}
+	
 	
 	@Override
 	public void draw(View view) {
 		DrawingUtils.clear(view, Color.hsb(0, 0, 0.2));
-
+		view.setStroke(Color.WHITE);
+		view.setLineWidth(2);
+		for (int i = 0; i < n; i++) {
+			view.strokeLine(getVertex(i), getVertex(i+1));
 		}
 	}
 	
