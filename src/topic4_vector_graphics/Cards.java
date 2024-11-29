4535b5f2906170ab36555017249803e2fccdabad
gitLMgit <115313459+gitLMgit@users.noreply.github.com>
2024-11-29 12:53:26 +0100

    Update Cards.java

--- src/topic4_vector_graphics/Cards.java
+++ src/topic4_vector_graphics/Cards.java
@@ -27,6 +27,21 @@ public class Cards implements Drawing {
 
 	
 	void drawCard(View view) {
+		// Okvir karte sa senkom
+		
+		view.setFill(Color.WHITE);
+		view.setEffect(new DropShadow(32, Color.BLACK));          // Efekt se odnosi na svaki naredni iscrtani objekat sve dok ne postavimo efekt na null.
+		view.fillRoundRectCentered(Vector.ZERO, size.div(2), a);
+		view.setEffect(null);
+
+		// Simbol na karti
+		
+		view.setLineCap(StrokeLineCap.BUTT);
+		view.setStroke(Color.hsb(0, 0.9, 0.9));
+		view.setLineWidth(16);
+		
+		view.strokeLine(Vector.polar(r, 1.0 / 8), Vector.polar(r, 5.0 / 8));
+		view.strokeCircleCentered(Vector.ZERO, r);
 	}
 	
 	
@@ -34,7 +49,21 @@ public class Cards implements Drawing {
 	public void draw(View view) {
 		view.setTransformation(Transformation.translation(new Vector(-120, -120)));
 		DrawingUtils.clear(view, Color.hsb(120, 0.5, 0.2));
-	
+		
+		RNG rng = new RNG(6380977105788498275l);
+
+		for (int i = nCards - 1; i >= 0; i--) {
+			view.stateStore();
+			
+			Transformation t = new Transformation()
+					.rotate(0.01 * rng.nextGaussian())
+					.translate(d.mul(i));
+			
+			view.addTransformation(t);
+			drawCard(view);
+			
+			view.stateRestore();
+		}
 	}
 	
 	
